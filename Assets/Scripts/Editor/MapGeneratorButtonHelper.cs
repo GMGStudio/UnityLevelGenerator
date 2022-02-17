f09803ee444f720d490fd721c4a498c2c05727f4
BjoernTheDev <mail@bjoern-hempel.de>
2022-02-17 17:40:38 +0100

    Initial Commit

---
+++ Assets/Scripts/Editor/MapGeneratorButtonHelper.cs
@@ -0,0 +1,22 @@
+using UnityEngine;
+using System.Collections;
+using UnityEditor;
+
+[CustomEditor(typeof(MapObjectGenerator))]
+public class MapGenerator : Editor
+{
+    public override void OnInspectorGUI()
+    {
+        DrawDefaultInspector();
+
+        MapObjectGenerator myScript = (MapObjectGenerator)target;
+        if (GUILayout.Button("Generate Map"))
+        {
+            myScript.GenerateMap();
+        }
+        if (GUILayout.Button("Delete Objects"))
+        {
+            myScript.DeleteObjects();
+        }
+    }
+}
---
+++ Assets/Scripts/Editor/MapObjectGenerator.cs
@@ -0,0 +1,68 @@
+using UnityEngine;
+using System.Collections;
+using System.Collections.Generic;
+using System.Linq;
+
+[RequireComponent(typeof(MeshFilter))]
+public class MapObjectGenerator : MonoBehaviour
+{
+    [SerializeField]
+    private List<GameObject> rocks = null;
+    [SerializeField]
+    private List<GameObject> trees = null;
+    [SerializeField]
+    private int maxTrees;
+    [SerializeField]
+    private int maxRocks;
+
+
+    internal void GenerateMap()
+    {
+        SpawnObjects(trees, maxTrees, true);
+        SpawnObjects(rocks, maxRocks);
+    }
+
+    private void SpawnObjects(List<GameObject> gameObjects,int amount,bool rotate = false)
+    {
+        for (int i = 0; i < amount; i++)
+        {
+            GameObject prefab = GetRandomObject(gameObjects);
+            GameObject spawned = Instantiate(prefab, getRandomPointOnGround(), prefab.transform.localRotation, gameObject.transform);
+            spawned.transform.localScale = Vector3.one;
+            if (rotate) {
+                spawned.transform.eulerAngles = new Vector3(spawned.transform.rotation.eulerAngles.x, Random.Range(1f, 90f), spawned.transform.rotation.eulerAngles.z);
+            }
+        }
+    }
+
+    internal void DeleteObjects()
+    {
+        var tempList = transform.Cast<Transform>().ToList();
+        foreach (var child in tempList)
+        {
+            DestroyImmediate(child.gameObject);
+        }
+    }
+
+    private Vector3 getRandomPointOnGround()
+    {
+        List<Vector3> VerticeList = new List<Vector3>(gameObject.GetComponent<MeshFilter>().sharedMesh.vertices);
+        Vector3 leftTop = gameObject.transform.TransformPoint(VerticeList[0]);
+        Vector3 rightTop = gameObject.transform.TransformPoint(VerticeList[10]);
+        Vector3 leftBottom = gameObject.transform.TransformPoint(VerticeList[110]);
+        Vector3 rightBottom = gameObject.transform.TransformPoint(VerticeList[120]);
+        Vector3 XAxis = rightTop - leftTop;
+        Vector3 ZAxis = leftBottom - leftTop;
+        Vector3 position = leftTop + XAxis * Random.value + ZAxis * Random.value;
+        position.y = 0;
+        return position;
+
+    }
+
+    private GameObject GetRandomObject(List<GameObject> list)
+    {
+
+        return list[Random.Range(0, list.Count)];
+    }
+
+}
